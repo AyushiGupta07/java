a8aa7c84dd3023cccb99ad7693c5202cc11e715c
AyushiGupta07 <chanchalneekhra@gmail.com>
2021-11-14 22:44:16 -0800

    Instance Block

---
+++ src/Instances.java
@@ -0,0 +1,15 @@
+public class Instances {
+    public static void main(String[] args) {
+
+        Instances obj1=new Instances();
+        Instances obj2=new Instances();
+        Instances obj3=new Instances();
+        System.out.println(Instances.count);
+
+    }
+    public static int count;
+     {
+        count++;
+    }
+
+}
