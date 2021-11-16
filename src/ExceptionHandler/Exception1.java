813a7da705253fa16162d2f4162c2c4f8fa479b7
AyushiGupta07 <chanchalneekhra@gmail.com>
2021-11-16 00:58:25 -0800

    Exception

---
+++ src/ExceptionHandler/Exception1.java
@@ -0,0 +1,26 @@
+package ExceptionHandler;
+
+import java.util.Scanner;
+
+public class Exception1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+        try {
+            System.out.println(" enter the number u want to enter ");
+            int a = sc.nextInt();
+            System.out.println(" the array element at index " + a + "=" + arr[a] + " ");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+         //   System.out.println(e);
+          //  e.printStackTrace();
+            System.out.println(e.toString());
+        }
+
+
+    }
+}
