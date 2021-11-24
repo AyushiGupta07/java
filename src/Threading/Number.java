54e83c9f9d12fb4e4c9da3406f7813a67ea6cb5d
AyushiGupta07 <chanchalneekhra@gmail.com>
2021-11-23 19:21:27 -0800

    create a thread which sleeps for 5000 milliseconds

---
+++ src/Threading/Number.java
@@ -0,0 +1,34 @@
+package Threading;
+import java.util.Scanner;
+public class Number extends Thread {
+    public int n;
+    Thread t = new Thread();
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the number ");
+        n = sc.nextInt();
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i + " ");
+        }
+        try {
+            t.sleep(5000);
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
+
+
+        for (int i = 6; i <= n; i++) {
+            System.out.println(i + " ");
+        }
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+ //       Thread t=new Thread();
+        Number obj=new Number();
+        obj.start();
+    }
+}
+
